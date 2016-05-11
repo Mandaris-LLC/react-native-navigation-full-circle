68bdaaab7aeccf1e77621fc7742f85881322d2f3
Ran Greenberg <rang@wix.com>
2016-05-11 15:50:02 +0300

    export NavigationToolBarIOS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Navigation from './src/Navigation';
 import Screen from './src/Screen';
 import RctActivity from './src/modules/RctActivity';
+import { NavigationToolBarIOS } from 'react-native-controllers';
 
 export {
   Navigation,
   Screen,
-  RctActivity
+  RctActivity,
+  NavigationToolBarIOS
 };
