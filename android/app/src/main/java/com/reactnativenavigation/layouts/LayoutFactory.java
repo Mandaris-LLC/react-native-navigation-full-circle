d59639008025f37cf760e445a720e2ee0c86eece
Daniel Zlotin <danielzl@wix.com>
2016-07-17 19:14:59 +0300

    Layout

--- android/app/src/main/java/com/reactnativenavigation/layouts/LayoutFactory.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/LayoutFactory.java
@@ -1,10 +1,13 @@
 package com.reactnativenavigation.layouts;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Gravity;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
@@ -15,7 +18,7 @@ public class LayoutFactory {
     }
 
     public static class BottomTabsParams {
-
+        private boolean enabled;
     }
 
     public static class Params {
@@ -27,9 +30,22 @@ public class LayoutFactory {
         LinearLayout root = createRoot(activity);
         FrameLayout content = createContent(activity);
         if (params.sideMenu.enabled) {
-            createSideMenu(activity, content);
+            DrawerLayout sideMenu = createSideMenu(activity, content);
+            root.addView(sideMenu, new LinearLayout.LayoutParams(MATCH_PARENT, 0, 1));
+        } else {
+            root.addView(content, new LinearLayout.LayoutParams(MATCH_PARENT, 0, 1));
         }
+        if (params.bottomTabs.enabled) {
+            AHBottomNavigation bottomTabs = createBottomTabs(activity);
+            root.addView(bottomTabs, new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        }
+
         return null;
+    }
+
+    @NonNull
+    private static AHBottomNavigation createBottomTabs(Activity activity) {
+        return new AHBottomNavigation(activity);
     }
 
     private static LinearLayout createRoot(Activity activity) {
@@ -38,17 +54,17 @@ public class LayoutFactory {
         return root;
     }
 
-    private static void createSideMenu(Activity activity, FrameLayout content) {
+    private static DrawerLayout createSideMenu(Activity activity, FrameLayout content) {
         DrawerLayout drawerLayout = new DrawerLayout(activity);
         FrameLayout drawerContent = new FrameLayout(activity);
         drawerLayout.addView(content, new DrawerLayout.LayoutParams(MATCH_PARENT, 0, 1));
         DrawerLayout.LayoutParams drawerContentParams = new DrawerLayout.LayoutParams(WRAP_CONTENT, MATCH_PARENT);
         drawerContentParams.gravity = Gravity.START;
         drawerLayout.addView(drawerContent, drawerContentParams);
+        return drawerLayout;
     }
 
     private static FrameLayout createContent(Activity activity) {
-        FrameLayout content = new FrameLayout(activity);
-        return content;
+        return new FrameLayout(activity);
     }
 }
