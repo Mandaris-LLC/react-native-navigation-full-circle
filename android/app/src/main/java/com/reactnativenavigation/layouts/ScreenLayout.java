94519d37ba058cfa3599ffc861a2d5d4406c66d9
Guy Carmeli <guyc@wix.com>
2016-07-18 17:25:57 +0300

    TopBar

--- android/app/src/main/java/com/reactnativenavigation/layouts/ScreenLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/ScreenLayout.java
@@ -1,17 +1,51 @@
 package com.reactnativenavigation.layouts;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.widget.LinearLayout;
 
-public class ScreenLayout extends LinearLayout {
+import com.facebook.react.ReactInstanceManager;
+import com.reactnativenavigation.views.ContentView;
+import com.reactnativenavigation.views.ScrollDirectionListener;
 
-    public static class Params {
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
+public class ScreenLayout extends LinearLayout implements ScrollDirectionListener.OnScrollChanged {
+
+    private final ReactInstanceManager reactInstanceManager;
+    private final String moduleName;
+    private final Bundle passProps;
+    private ContentView contentView;
+    private TopBar topBar;
+
+    public ScreenLayout(Context context, ReactInstanceManager reactInstanceManager, String moduleName, Bundle passProps) {
+        super(context);
+        this.reactInstanceManager = reactInstanceManager;
+        this.moduleName = moduleName;
+        this.passProps = passProps;
+        setOrientation(VERTICAL);
+
+        createViews();
     }
 
-    public ScreenLayout(Context context, Params params) {
-        super(context);
-        setOrientation(VERTICAL);
-        addView();
+    private void createViews() {
+        addTopBar();
+        addContentView();
+    }
+
+    private void addTopBar() {
+        topBar = new TopBar(getContext());
+        addView(topBar, new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+    }
+
+    private void addContentView() {
+        contentView = new ContentView(getContext(), reactInstanceManager, moduleName, passProps, this);
+        addView(contentView, new LayoutParams(MATCH_PARENT, MATCH_PARENT));
+    }
+
+    @Override
+    public void onScrollChanged(ScrollDirectionListener.Direction direction) {
+
     }
 }
---
+++ android/app/src/main/java/com/reactnativenavigation/layouts/TopBar.java
@@ -0,0 +1,12 @@
+package com.reactnativenavigation.layouts;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+
+public class TopBar extends AppBarLayout {
+
+    public TopBar(Context context) {
+        super(context);
+        setFitsSystemWindows(true);
+    }
+}
