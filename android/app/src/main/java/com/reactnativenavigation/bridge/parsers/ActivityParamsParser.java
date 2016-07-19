5d18f457578b6abad818ecebbdbe217e3f38e89a
Daniel Zlotin <danielzl@wix.com>
2016-07-19 19:47:30 +0300

    bundle converter

--- android/app/src/main/java/com/reactnativenavigation/bridge/parsers/ActivityParamsParser.java
+++ android/app/src/main/java/com/reactnativenavigation/bridge/parsers/ActivityParamsParser.java
@@ -1,4 +1,11 @@
 package com.reactnativenavigation.bridge.parsers;
 
+import com.facebook.react.bridge.ReadableMap;
+import com.reactnativenavigation.controllers.ActivityParams;
+
 public class ActivityParamsParser implements ParamsParser<ActivityParams> {
+    @Override
+    public ActivityParams parse(ReadableMap params) {
+        return null;
+    }
 }
