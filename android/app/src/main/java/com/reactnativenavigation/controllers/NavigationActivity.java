04b1993b8bd3ee69ff55098271c4af5848f7d39d
Daniel Zlotin <danielzl@wix.com>
2016-07-24 11:34:19 +0300

    fix compiling

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
@@ -45,7 +44,6 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
 
     private void createLayout() {
         singleScreenLayout = new SingleScreenLayout(this, activityParams.screenParams);
-        singleScreenLayout.createLayout();
         setContentView(singleScreenLayout);
     }
 
