0a265329d7a113c44e166271b3527f27bc16d82e
Rotem M <rotemm@wix.com>
2016-07-27 14:52:02 +0300

    TabbedScreen basic functionality

--- android/app/src/main/java/com/reactnativenavigation/screens/TabbedScreen.java
+++ android/app/src/main/java/com/reactnativenavigation/screens/TabbedScreen.java
@@ -19,7 +19,7 @@ import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class TabbedScreen extends Screen {
 
-    private List<ContentView> contentViews = new ArrayList<>();
+    private List<ContentView> contentViews;
     private ViewPager viewPager;
     private ViewPagerAdapter viewPagerAdapter;
 
@@ -30,20 +30,24 @@ public class TabbedScreen extends Screen {
     @Override
     protected void createContent() {
         TabLayout tabLayout = topBar.initTabs();
+        contentViews = new ArrayList<>();
         viewPager = new ViewPager(getContext());
         addView(viewPager);
-        viewPagerAdapter = new ViewPagerAdapter(getContext(), viewPager, contentViews);
-//        tabLayout.setupWithViewPager(viewPager);
 
         for (TopTabParams topTabParam : screenParams.topTabParams) {
-            ContentView contentView = new ContentView(getContext(), topTabParam.screenId, screenParams.passProps, screenParams.navigationParams, this);
+            ContentView contentView = new ContentView(getContext(),
+                    topTabParam.screenId,
+                    screenParams.passProps,
+                    screenParams.navigationParams,
+                    this);
             viewPager.addView(contentView, addBelowTopBar());
             contentView.init();
-
-            tabLayout.addTab(tabLayout.newTab().setText(topTabParam.title));
-//            viewPagerAdapter.addTabPage(topTabParam.title);
+            contentViews.add(contentView);
         }
 
+        viewPagerAdapter = new ViewPagerAdapter(viewPager, contentViews, screenParams.topTabParams);
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @NonNull
@@ -72,56 +76,20 @@ public class TabbedScreen extends Screen {
 
     public class ViewPagerAdapter extends PagerAdapter implements TabLayout.OnTabSelectedListener, ViewPager.OnPageChangeListener {
 
-        private static final String EVENT_ON_TAB_SELECTED = "OnTabSelected";
-
-        private ViewPager mViewPager;
-        //        private final ArrayList<ScreenStack> mScreenStacks;
-        private final ArrayList<String> mNavigatorIds;
+        private ViewPager viewPager;
         private List<ContentView> contentViews;
-        //        private final Map<String, ScreenStack> mStackByNavigatorId;
-        private int mCurrentPage = 0;
+        private List<TopTabParams> topTabParams;
 
-        public ViewPagerAdapter(Context context, ViewPager viewPager, List<ContentView> contentViews) {
-            this.mViewPager = viewPager;
+        public ViewPagerAdapter(ViewPager viewPager, List<ContentView> contentViews, List<TopTabParams> topTabParams) {
+            this.viewPager = viewPager;
             this.contentViews = contentViews;
-//            mScreenStacks = new ArrayList<>();
-            this.mNavigatorIds = new ArrayList<>();
-//            mStackByNavigatorId = new HashMap<> ();
-//            for (ContentView contentView : contentViews) {
-//                ScreenStack stack = new ScreenStack(context);
-//                stack.push(screen);
-//                mScreenStacks.add(stack);
-//                mNavigatorIds.add(screen.navigatorId);
-//                mStackByNavigatorId.put(screen.navigatorId, stack);
-//            }
+            this.topTabParams = topTabParams;
         }
 
-//        public void push(Screen screen) {
-//            ScreenStack stack = mStackByNavigatorId.get(screen.navigatorId);
-//            Screen prevScreen = mScreenStacks.get(mCurrentPage).peek();
-//            mToolbar.setupToolbarButtonsAsync(prevScreen, screen);
-//            stack.push(screen);
-//        }
-//
-//        public Screen pop(String navigatorId) {
-//            ScreenStack stack = mStackByNavigatorId.get(navigatorId);
-//            Screen oldScreen =  stack != null ? stack.pop() : null;
-//            Screen newScreen = stack.peek();
-//            mToolbar.setupToolbarButtonsAsync(oldScreen, newScreen);
-//            return oldScreen;
-//        }
-//
-//        public Screen peek(String navigatorId) {
-//            ScreenStack stack = mStackByNavigatorId.get(navigatorId);
-//            return stack != null ? stack.peek() : null;
-//        }
-
-//        @Override
-//        public Object instantiateItem(ViewGroup container, int position) {
-//            ScreenStack view = mScreenStacks.get(position);
-//            container.addView(view);
-//            return view;
-//        }
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return contentViews.get(position);
+        }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object view) {
@@ -140,31 +108,13 @@ public class TabbedScreen extends Screen {
 
         @Override
         public CharSequence getPageTitle(int position) {
-//            return contentViews.get(position).peek().label;
-            return "";
+            return topTabParams.get(position).title;
         }
 
         @Override
         public void onTabSelected(TabLayout.Tab tab) {
-            // Set the viewPager's current item
             int position = tab.getPosition();
-            mViewPager.setCurrentItem(position);
-
-//            // Set screen buttons
-//            Screen prevScreen = mScreenStacks.get(mCurrentPage).peek();
-//            Screen newScreen = mScreenStacks.get(position).peek();
-//            mToolbar.setupToolbarButtonsAsync(prevScreen, newScreen);
-//
-//            // Set title
-//            mToolbar.setTitle(newScreen.title == null ? "" : newScreen.title);
-//
-//            // Set navigation color
-//            StyleHelper.updateStyles(mToolbar, newScreen);
-//
-//            // Send tab selected event
-//            WritableMap params = Arguments.createMap();
-//            Screen screen = mScreenStacks.get(position).peek();
-//            RctManager.getInstance().sendEvent(EVENT_ON_TAB_SELECTED, screen, params);
+            viewPager.setCurrentItem(position);
         }
 
         @Override
@@ -177,16 +127,6 @@ public class TabbedScreen extends Screen {
 
         }
 
-        public String getNavigatorId(int position) {
-            return mNavigatorIds.get(position);
-        }
-
-        public int getStackSizeForNavigatorId(String activeNavigatorID) {
-//            return mStackByNavigatorId.get(activeNavigatorID).getStackSize();
-            return 0;
-        }
-
-
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -194,7 +134,7 @@ public class TabbedScreen extends Screen {
 
         @Override
         public void onPageSelected(int position) {
-            mCurrentPage = position;
+
         }
 
         @Override
