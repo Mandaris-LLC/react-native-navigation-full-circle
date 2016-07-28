702687836c64b677e7d4ac42e50883b7b1cc2cf5
Guy Carmeli <guyc@wix.com>
2016-07-28 22:42:31 +0300

    Remove hardcoded TitleBar icon dimensions

--- android/app/src/main/java/com/reactnativenavigation/react/JsDevImageLoader.java
+++ android/app/src/main/java/com/reactnativenavigation/react/JsDevImageLoader.java
@@ -8,7 +8,6 @@ import android.os.StrictMode;
 import android.support.annotation.NonNull;
 
 import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.utils.ViewUtils;
 
 import java.io.IOException;
 import java.net.URL;
@@ -33,10 +32,6 @@ public class JsDevImageLoader {
     private static Drawable tryLoadIcon(String iconDevUri) throws IOException {
         URL url = new URL(iconDevUri);
         Bitmap bitmap = BitmapFactory.decodeStream(url.openStream());
-        final int dimensions = (int) ViewUtils.convertDpToPixel(48);
-        // TODO: fix hard coded dimensions -add options to decodeStream
-        bitmap = Bitmap.createScaledBitmap(bitmap, dimensions, dimensions, false);
         return new BitmapDrawable(NavigationApplication.instance.getResources(), bitmap);
     }
-
 }
