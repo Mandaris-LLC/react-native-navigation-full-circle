cc87b8513b2e334e8bfd3a725ea6bf48170ce5e8
Guy Carmeli <guyc@wix.com>
2016-08-07 19:13:48 +0300

    Add option to set custom splash layout

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -2,6 +2,7 @@ package com.reactnativenavigation.controllers;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,9 +15,27 @@ public abstract class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         NavigationApplication.instance.startReactContext();
-        //TODO show fancy splash
+        setSplashLayout();
+    }
+
+    private void setSplashLayout() {
+        final int splashLayout = getSplashLayout();
+        if (splashLayout > 0) {
+            setContentView(splashLayout);
+        } else {
+            setDefaultSplashLayout();
+        }
+    }
+
+    private void setDefaultSplashLayout() {
         View view = new View(this);
         view.setBackgroundColor(Color.WHITE);
         setContentView(view);
     }
+
+    /**
+     * 
+     * @return -1 if you don't need a splash layout
+     */
+    public abstract @LayoutRes int getSplashLayout();
 }
