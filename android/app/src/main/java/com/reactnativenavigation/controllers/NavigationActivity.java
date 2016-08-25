d3582d27804e115fd0d3dfa43b03d2ba22d46d29
Guy Carmeli <guyc@wix.com>
2016-08-25 15:51:58 +0300

    Call recreate instead of finish and startActivity

--- android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/NavigationActivity.java
@@ -17,7 +17,6 @@ import com.reactnativenavigation.params.TitleBarLeftButtonParams;
 import com.reactnativenavigation.react.JsDevReloadHandler;
 import com.reactnativenavigation.react.ReactGateway;
 import com.reactnativenavigation.react.RedboxPermission;
-import com.reactnativenavigation.utils.IntentUtils;
 
 import java.util.List;
 
@@ -44,13 +43,11 @@ public class NavigationActivity extends AppCompatActivity implements DefaultHard
 
         if (!NavigationApplication.instance.isReactContextInitialized()) {
             waitingForNewJsContext = true;
-            finish();
-            startActivity(IntentUtils.getLauncherIntent());
+            recreate();
             return;
         }
 
         RedboxPermission.permissionToShowRedboxIfNeeded(this);
-
         activityParams = NavigationCommandsHandler.parseActivityParams(getIntent());
 
         createLayout();
