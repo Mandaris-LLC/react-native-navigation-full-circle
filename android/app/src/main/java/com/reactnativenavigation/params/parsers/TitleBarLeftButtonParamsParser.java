41a87eeda997a85eb65d5f85bfd79f458bec61ed
Guy Carmeli <guyc@wix.com>
2016-08-31 09:01:01 +0300

    Clarify unsupported left button exception message

--- android/app/src/main/java/com/reactnativenavigation/params/parsers/TitleBarLeftButtonParamsParser.java
+++ android/app/src/main/java/com/reactnativenavigation/params/parsers/TitleBarLeftButtonParamsParser.java
@@ -24,7 +24,7 @@ public class TitleBarLeftButtonParamsParser extends TitleBarButtonParamsParser {
             case "sideMenu":
                 return MaterialMenuDrawable.IconState.BURGER;
             default:
-                throw new RuntimeException("Unsupported button type " + id);
+                throw new RuntimeException("Unsupported button id: " + id + ". Supported ids are: back, cancel, accept and sideMenu");
         }
     }
 }
