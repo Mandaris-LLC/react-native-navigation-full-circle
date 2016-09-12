a4c91c1eed29e9c539ff6e39da27cedbc3f1db09
Daniel Zlotin <danielzl@wix.com>
2016-09-12 19:25:44 +0300

    gradle fixes

--- android/app/src/test/java/com/reactnativenavigation/EnvironmentTest.java
+++ android/app/src/test/java/com/reactnativenavigation/EnvironmentTest.java
@@ -10,6 +10,6 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class EnvironmentTest {
     @Test
     public void assertJ() {
-        assertThat(1 + 2).isEqualTo(3);
+        assertThat(1 + 2).isEqualTo(3).isGreaterThan(2).isLessThan(4).isNotNegative().isPositive().isNotZero();
     }
 }
