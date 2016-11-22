afa58226b057b6d165089d02bb426a5ba5334a43
Daniel Zlotin <danielzl@wix.com>
2016-11-22 15:27:01 +0200

    trying installing only the tools

--- scripts/installAndroidSDK.sh
+++ scripts/installAndroidSDK.sh
@@ -2,10 +2,10 @@
 
 curl --location http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz | tar -x -z -C $HOME
 
-#( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui -a \
-#--filter \
-#tools,\
-#platform-tools,\
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui -a \
+--filter \
+tools,\
+platform-tools
 #build-tools-25.0.0,\
 #android-23,\
 #addon-google_apis-google-23,\
