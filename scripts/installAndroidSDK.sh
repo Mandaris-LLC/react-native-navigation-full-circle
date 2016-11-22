ca02c38b01a8cce3a9eefac6263d0b8324af54d4
Daniel Zlotin <danielzl@wix.com>
2016-11-22 16:39:21 +0200

    removed manual sdk download script

--- scripts/installAndroidSDK.sh
+++ scripts/installAndroidSDK.sh
@@ -11,14 +11,3 @@ curl --location http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz | tar
 echo "Copying Android licenses"
 scriptdir=`dirname $0`
 cp "${scriptdir}"/android-licenses/* "${ANDROID_HOME}"/licenses
-
-#( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | android update sdk --no-ui -a \
-#--filter \
-#tools,\
-#platform-tools
-#build-tools-25.0.0,\
-#android-23,\
-#addon-google_apis-google-23,\
-#extra-android-m2repository,\
-#extra-google-google_play_services,\
-#extra-google-m2repository
