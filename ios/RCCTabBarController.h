84f30797c087aa6f5fd6b00c3478965afbcd25c8
Ran <gran33@gmail.com>
2016-12-28 14:11:31 +0200

    add tabBarDelegate -not really needed since RCCTabBarController is already delegate to him self (inheritss from UITabBarController). adding this for the record. (#604)

--- ios/RCCTabBarController.h
+++ ios/RCCTabBarController.h
@@ -1,7 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "RCTBridge.h"
 
-@interface RCCTabBarController : UITabBarController
+@interface RCCTabBarController : UITabBarController <UITabBarDelegate>
 
 - (instancetype)initWithProps:(NSDictionary *)props children:(NSArray *)children globalProps:(NSDictionary*)globalProps bridge:(RCTBridge *)bridge;
 - (void)performAction:(NSString*)performAction actionParams:(NSDictionary*)actionParams bridge:(RCTBridge *)bridge completion:(void (^)(void))completion;
