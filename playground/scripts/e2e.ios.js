c12720dc56538df43d790b36b556d57fc7691285
Daniel Zlotin <danielzl@wix.com>
2017-01-05 15:54:17 +0200

    preparing for detox

--- playground/scripts/e2e.ios.js
+++ playground/scripts/e2e.ios.js
@@ -12,18 +12,18 @@ function kill(process) {
   execSilent(`pkill -f "${process}" || true`);
 }
 
-function buildXcodeReleaseProj() {
-//exec(`RCT_NO_LAUNCH_PACKAGER=true cd ios && xcodebuild \
-//-scheme example_Detox clean build \
-//-project example.xcodeproj \
-//-destination "platform=iOS Simulator,name=iPhone 7,OS=10.1" \
-//-derivedDataPath ./DerivedData/example`);
+function buildProjForDetox() {
+  exec(`RCT_NO_LAUNCH_PACKAGER=true \
+          cd ios && xcodebuild \
+            -scheme playground_Detox clean build \
+            -project playground.xcodeproj \
+            -sdk iphonesimulator \
+            -derivedDataPath ./DerivedData/playground`);
 }
 
 function e2e() {
-  kill(`detox-server`);
-
   try {
+    kill(`detox-server`);
     cp.exec(`./node_modules/.bin/detox-server > ./detox-server.log 2>&1`);
     exec(`BABEL_ENV=test ./node_modules/mocha/bin/mocha e2e --opts ./e2e/mocha.opts`);
   } finally {
@@ -31,14 +31,14 @@ function e2e() {
     kill(`Simulator`);
     kill(`CoreSimulator`);
     exec(`cat ./detox-server.log`);
+    exec(`rm -f ./detox-server.log`);
     exec(`sleep 5`);
   }
 }
 
 function run() {
-  buildXcodeReleaseProj();
+  buildProjForDetox();
   e2e();
 }
 
 run();
-
