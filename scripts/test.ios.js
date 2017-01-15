2db23754dd7c4f150f5b44f8a4601a4efba10af3
Daniel Zlotin <danielzl@wix.com>
2017-01-15 16:33:32 +0200

    run e2e on travis

--- scripts/test.ios.js
+++ scripts/test.ios.js
@@ -2,6 +2,9 @@ const exec = require('shell-utils').exec;
 
 function run() {
   process.chdir('./playground');
+  exec.execSync(`hardlink node_modules/react-native-navigation/ -u || true`);
+  exec.execSync(`yarn install`);
+  exec.execSync(`yarn run e2e release`);
   process.chdir('../');
 }
 
