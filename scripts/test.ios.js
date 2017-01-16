a5e303f681e5804b673f254375e6083f1d353e5b
Daniel Zlotin <danielzl@wix.com>
2017-01-16 11:05:43 +0200

    Revert "trying to fix travis"
    This reverts commit f62cce4897afef8ac9c8ab89bd1f72ee051b3a17.

--- scripts/test.ios.js
+++ scripts/test.ios.js
@@ -1,9 +1,11 @@
 const exec = require('shell-utils').exec;
 
 function run() {
-  exec.execSync(`cd playground && hardlink node_modules/react-native-navigation/ -u || true`);
-  exec.execSync(`cd playground && yarn install`);
-  //exec.execSync(`cd playground && yarn run e2e release`);
+  process.chdir('./playground');
+  exec.execSync(`hardlink node_modules/react-native-navigation/ -u || true`);
+  exec.execSync(`yarn install`);
+  exec.execSync(`yarn run e2e release`);
+  process.chdir('../');
 }
 
 run();
