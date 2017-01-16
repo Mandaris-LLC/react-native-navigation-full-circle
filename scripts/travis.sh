4f192b29dcb44b34cae3144057873bd40d29d67e
Daniel Zlotin <danielzl@wix.com>
2017-01-16 14:53:10 +0200

    fixed travis script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-color='\033[1;36m'
+lightCyan='\033[1;36m'
+green='\033[0;32m'
 nocolor='\033[0m'
 
 run_f () {
@@ -8,11 +9,15 @@ run_f () {
   name=${cmd//[ ]/_}
 
   echo "travis_fold:start:$name"
-  echo -e "${color}\t $cmd \t${nocolor}"
+  echo -e "${lightCyan}\t\t $cmd ${nocolor}"
+  SECONDS=0
 
   ($cmd)
 
+  duration=$SECONDS
   echo "travis_fold:end:$name"
+  echo -e "${green}\t\t\t --> $(($duration / 60)) minutes and $(($duration % 60)) seconds ${nocolor}"
+  echo "\n"
 }
 
 run_f "yarn install"
