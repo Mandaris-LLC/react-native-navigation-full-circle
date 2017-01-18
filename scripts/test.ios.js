da7fe3a584fd849961d18003eaefbda5d6920532
Daniel Zlotin <danielzl@wix.com>
2017-01-18 16:49:35 +0200

    fixing install script

--- scripts/test.ios.js
+++ scripts/test.ios.js
@@ -3,8 +3,6 @@ const exec = require('shell-utils').exec;
 function run() {
   process.chdir('./playground');
   exec.execSync(`hardlink node_modules/react-native-navigation/ -u || true`);
-  const yarnCacheDir = exec.execSyncRead(`yarn cache dir`);
-  exec.execSync(`rm -rf ${yarnCacheDir}/*navigation*`);
   exec.execSync(`yarn install`);
   exec.execSync(`yarn run e2e release`);
   process.chdir('../');
