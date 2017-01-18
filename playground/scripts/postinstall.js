b1a138f08005b002f388446ee8a4a5281ffc9d1e
Daniel Zlotin <danielzl@wix.com>
2017-01-18 12:37:11 +0200

    fixed script

--- playground/scripts/postinstall.js
+++ playground/scripts/postinstall.js
@@ -1,6 +1,6 @@
 const shellUtils = require('shell-utils');
 
-shellUtils.exec.execSync(`rm yarn.lock`);
+shellUtils.exec.execSync(`rm yarn.lock || true`);
+shellUtils.exec.execSync(`./scripts/ignoreReactWarnings.rb`);
 shellUtils.exec.execSync(`rm -rf node_modules/react-native-navigation/node_modules`);
 shellUtils.exec.execSync(`rm -rf node_modules/react-native-navigation/playground`);
-shellUtils.exec.execSync(`./scripts/ignoreReactWarnings.rb`);
