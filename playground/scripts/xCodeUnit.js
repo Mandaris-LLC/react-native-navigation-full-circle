3d3d82dbf21feef9c9be85faed5bbea73609b05e
Daniel Zlotin <danielzl@wix.com>
2017-02-09 12:46:56 +0200

    xcode unit tests

---
+++ playground/scripts/xCodeUnit.js
@@ -0,0 +1,34 @@
+const _ = require('lodash');
+const shellUtils = require('shell-utils');
+const fs = require('fs');
+
+function testProject() {
+  shellUtils.exec.execSync(`echo 'travis_fold:start:xcodeunit'`);
+  const cmd = `RCT_NO_LAUNCH_PACKAGER=true
+          cd ios && xcodebuild
+            test
+            -scheme "playground"
+            -project playground.xcodeproj
+            -destination 'platform=iOS Simulator,name=iPhone 7'
+            `;
+
+  if (hasXcpretty()) {
+    shellUtils.exec.execSync(`${cmd} | xcpretty && exit \${PIPESTATUS[0]}`);
+  } else {
+    shellUtils.exec.execSync(`${cmd}`);
+  }
+  shellUtils.exec.execSync(`echo 'travis_fold:end:xcodeunit'`);
+}
+function hasXcpretty() {
+  try {
+    return shellUtils.exec.execSyncRead(`which xcpretty`);
+  } catch (e) {
+    return false;
+  }
+}
+
+function run() {
+  testProject();
+}
+
+run();
--- scripts/test.ios.js
+++ scripts/test.ios.js
@@ -3,6 +3,7 @@ const exec = require('shell-utils').exec;
 function run() {
   process.chdir('./playground');
   exec.execSync(`yarn run quickinstall`);
+  exec.execSync(`yarn run xcodeunit`);
   exec.execSync(`yarn run e2e release`);
   process.chdir('../');
 }
