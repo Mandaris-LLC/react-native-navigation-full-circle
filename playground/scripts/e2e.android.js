ca13ad1885602f15a276e9f24c449f3a2ef87137
Daniel Zlotin <danielzl@wix.com>
2017-02-13 16:32:55 +0200

    trying to run android emulator

--- playground/scripts/e2e.android.js
+++ playground/scripts/e2e.android.js
@@ -2,20 +2,28 @@ const _ = require('lodash');
 const shellUtils = require('shell-utils');
 const fs = require('fs');
 
-const release = _.includes(process.argv, 'release');
+// function runEmulator() {
+//   shellUtils.exec.execSync(`echo no | android create avd --force -n test -t android-25 --abi armeabi-v7a`);
+//   shellUtils.exec.execSync(`emulator -avd test -no-skin -no-audio -no-window &`);
+//   shellUtils.exec.execSync(`android-wait-for-emulator`);
+//   shellUtils.exec.execSync(`adb shell input keyevent 82 &`);
+// }
 
-function e2e() { //eslint-disable-line
-  try {
-    shellUtils.exec.execSync(`echo 'travis_fold:start:android-espresso'`);
-    shellUtils.exec.execSync(`cd android && ./gradlew uninstallAll`);
-    shellUtils.exec.execSync(`cd android && ./gradlew connectedDebugAndroidTest`);
-  } finally {
-    shellUtils.exec.execSync(`echo 'travis_fold:end:android-espresso'`);
-  }
+function e2e() {
+  shellUtils.exec.execSync(`cd android && ./gradlew uninstallAll`);
+  shellUtils.exec.execSync(`cd android && ./gradlew connectedDebugAndroidTest`);
 }
 
 function run() {
-  e2e();
+  shellUtils.exec.execSync(`echo 'travis_fold:start:android-espresso'`);
+  try {
+    // if (process.env.CI) {
+    // runEmulator();
+    // }
+    e2e();
+  } finally {
+    shellUtils.exec.execSync(`echo 'travis_fold:end:android-espresso'`);
+  }
 }
 
 run();
