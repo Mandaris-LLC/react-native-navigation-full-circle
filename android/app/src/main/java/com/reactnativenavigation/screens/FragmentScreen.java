934c3fae51813780df986302711b1916402af9c1
Stefano Acerbetti <acerbetti@gmail.com>
2017-02-16 11:50:29 -0800

    Fix possible crash (#771)
    Crash description here
    http://stackoverflow.com/questions/7575921/illegalstateexception-can-not-perform-this-action-after-onsaveinstancestate-wit

--- android/app/src/main/java/com/reactnativenavigation/screens/FragmentScreen.java
+++ android/app/src/main/java/com/reactnativenavigation/screens/FragmentScreen.java
@@ -75,14 +75,14 @@ public class FragmentScreen extends Screen {
         FragmentManager fm = activity.getFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.add(R.id.fragment_screen_content, fragment);
-        transaction.commit();
+        transaction.commitAllowingStateLoss();
     }
 
     private void addSupportFragment(android.support.v4.app.Fragment supportFragment) {
         android.support.v4.app.FragmentManager fm = activity.getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction transaction = fm.beginTransaction();
         transaction.add(R.id.fragment_screen_content, supportFragment);
-        transaction.commit();
+        transaction.commitAllowingStateLoss();
     }
 
     @Nullable
