1ddaaff66500751b2d0795a5f5a6f97cce9ecae2
Guns <guns2410@users.noreply.github.com>
2017-02-16 15:58:42 +0530

    Ability to pass options to Redux Provider (#636)
    * Support for ApolloStack
    
    * Pass options to redux Provider

--- src/Navigation.js
+++ src/Navigation.js
@@ -14,9 +14,9 @@ function registerScreen(screenID, generator) {
   AppRegistry.registerComponent(screenID, generator);
 }
 
-function registerComponent(screenID, generator, store = undefined, Provider = undefined) {
+function registerComponent(screenID, generator, store = undefined, Provider = undefined, options = {}) {
   if (store && Provider) {
-    return _registerComponentRedux(screenID, generator, store, Provider);
+    return _registerComponentRedux(screenID, generator, store, Provider, options);
   } else {
     return _registerComponentNoRedux(screenID, generator);
   }
@@ -57,7 +57,7 @@ function _registerComponentNoRedux(screenID, generator) {
   return generatorWrapper;
 }
 
-function _registerComponentRedux(screenID, generator, store, Provider) {
+function _registerComponentRedux(screenID, generator, store, Provider, options) {
   const generatorWrapper = function() {
     const InternalComponent = generator();
     return class extends Screen {
@@ -79,7 +79,7 @@ function _registerComponentRedux(screenID, generator, store, Provider) {
 
       render() {
         return (
-          <Provider store={store}>
+          <Provider store={store} {...options}>
             <InternalComponent testID={screenID} navigator={this.navigator} {...this.state.internalProps} />
           </Provider>
         );
