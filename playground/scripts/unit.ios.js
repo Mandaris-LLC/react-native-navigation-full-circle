91ed603d96937ea5ee9aaefd4d8fd5f1653a3903
Daniel Zlotin <danielzl@wix.com>
2017-03-08 20:12:08 +0200

    try to solve apple bug

--- playground/scripts/unit.ios.js
+++ playground/scripts/unit.ios.js
@@ -2,21 +2,29 @@ const _ = require('lodash');
 const shellUtils = require('shell-utils');
 const fs = require('fs');
 
+function runWithXcprettyIfPossible(cmd) {
+  if (hasXcpretty()) {
+    shellUtils.exec.execSync(`${cmd} | xcpretty && exit \${PIPESTATUS[0]}`);
+  } else {
+    shellUtils.exec.execSync(`${cmd}`);
+  }
+}
+
 function testProject() {
   shellUtils.exec.execSync(`echo 'travis_fold:start:xcodeunit'`);
-  const cmd = `RCT_NO_LAUNCH_PACKAGER=true
+  runWithXcprettyIfPossible(`RCT_NO_LAUNCH_PACKAGER=true
+          cd ios && xcodebuild
+            build
+            -scheme "playground"
+            -project playground.xcodeproj
+            -sdk iphonesimulator
+            -configuration Debug`);
+  runWithXcprettyIfPossible(`RCT_NO_LAUNCH_PACKAGER=true
           cd ios && xcodebuild
             test
             -scheme "playground"
             -project playground.xcodeproj
-            -destination 'platform=iOS Simulator,name=iPhone 7'
-            `;
-
-  // if (hasXcpretty()) {
-  //   shellUtils.exec.execSync(`${cmd} | xcpretty && exit \${PIPESTATUS[0]}`);
-  // } else {
-  shellUtils.exec.execSync(`${cmd}`);
-  // }
+            -destination 'platform=iOS Simulator,name=iPhone 7'`);
   shellUtils.exec.execSync(`echo 'travis_fold:end:xcodeunit'`);
 }
 function hasXcpretty() {
