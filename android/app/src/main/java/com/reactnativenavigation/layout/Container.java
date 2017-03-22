9fbc6f30f8c01796eb48b804469ab7c137411f8e
Guy Carmeli <guyc@wix.com>
2017-03-22 08:46:20 +0200

    containerOnStop/Start - half way there

--- android/app/src/main/java/com/reactnativenavigation/layout/Container.java
+++ android/app/src/main/java/com/reactnativenavigation/layout/Container.java
@@ -1,11 +1,36 @@
 package com.reactnativenavigation.layout;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.FrameLayout;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationEventEmitter;
+
 public class Container extends FrameLayout {
-	public Container(Context context, LayoutFactory.ReactRootViewCreator reactRootViewCreator, String id, String name) {
+    private static final String TAG = "Container";
+    private String id;
+
+    public Container(Context context, LayoutFactory.ReactRootViewCreator reactRootViewCreator, String id, String name) {
 		super(context);
-		addView(reactRootViewCreator.create(id, name));
+        this.id = id;
+        addView(reactRootViewCreator.create(id, name));
+
 	}
+
+//    @Override
+//    protected void onAttachedToWindow() {
+//        Log.d(TAG, "onAttachedToWindow: " + id);
+//        super.onAttachedToWindow();
+//        NavigationEventEmitter.emit(NavigationApplication.instance.getReactNativeHost().getReactInstanceManager().getCurrentReactContext())
+//                .containerStart(id);
+//    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        Log.d(TAG, "onDetachedFromWindow: " + id);
+        super.onDetachedFromWindow();
+        NavigationEventEmitter.emit(NavigationApplication.instance.getReactNativeHost().getReactInstanceManager().getCurrentReactContext())
+                .containerStop(id);
+    }
 }
--- android/app/src/main/java/com/reactnativenavigation/react/NavigationEventEmitter.java
+++ android/app/src/main/java/com/reactnativenavigation/react/NavigationEventEmitter.java
@@ -7,9 +7,9 @@ import com.facebook.react.bridge.WritableMap;
 import static com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter;
 
 public class NavigationEventEmitter {
-	private static String onAppLaunched = "RNN.appLaunched";
-	private static String containerStart = "RNN.containerStart";
-	private static String containerStop = "RNN.containerStop";
+	private static final String onAppLaunched = "RNN.appLaunched";
+	private static final String containerStart = "RNN.containerStart";
+	private static final String containerStop = "RNN.containerStop";
 
 	public static NavigationEventEmitter emit(ReactContext context) {
 		return new NavigationEventEmitter(context);
@@ -26,7 +26,28 @@ public class NavigationEventEmitter {
 	}
 
 	private void emit(String eventName) {
-		WritableMap data = Arguments.createMap();
-		emitter.emit(eventName, data);
-	}
+        emit(eventName, Arguments.createMap());
+    }
+
+    private void emit(String eventName, WritableMap data) {
+        emitter.emit(eventName, data);
+    }
+
+    private void emit(String eventName, String param) {
+        emitter.emit(eventName, param);
+    }
+
+    public void containerStop(String id) {
+        WritableMap data = Arguments.createMap();
+        data.putString("id", id);
+//        emit(containerStop, data);
+        emit(containerStop, id);
+    }
+
+    public void containerStart(String id) {
+        WritableMap data = Arguments.createMap();
+        data.putString("id", id);
+//        emit(containerStart, data);
+        emit(containerStart, id);
+    }
 }
