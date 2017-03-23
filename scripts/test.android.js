00071c99c06007c88d0ef65936bc7410a53cb073
Daniel Zlotin <danielzl@wix.com>
2017-03-23 15:33:23 +0200

    android e2e works

--- scripts/test.android.js
+++ scripts/test.android.js
@@ -1,5 +1,16 @@
 const exec = require('shell-utils').exec;
 
+function e2e() {
+  try {
+    exec.execSync(`echo 'travis_fold:start:android-e2e'`);
+    exec.execSync(`yarn run uninstall-android`);
+    exec.execSync(`yarn run install-android`);
+    exec.execSync(`cd AndroidE2E && ./gradlew connectedDebugAndroidTest`);
+  } finally {
+    exec.execSync(`echo 'travis_fold:end:android-e2e'`);
+  }
+}
+
 function run() {
   if (process.env.CI) {
     exec.execSync(`./scripts/installAndroidSDK.sh`);
@@ -8,7 +19,7 @@ function run() {
   exec.execSync(`cd lib/android && ./gradlew clean testDebugUnitTest`);
 
   if (!process.env.CI) {
-    // exec.execSync(`cd playground && yarn run e2e-android`);
+    e2e();
   }
 }
 
