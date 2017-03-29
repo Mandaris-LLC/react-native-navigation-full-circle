0d2a3cc08ac55e3fc17277cebd67e1b98bb6bd8a
Daniel Zlotin <danielzl@wix.com>
2017-03-29 12:08:36 +0300

    removed jacoco

--- scripts/start.js
+++ scripts/start.js
@@ -8,22 +8,3 @@ function run() {
   exec.execSync(`adb reverse tcp:8081 tcp:8081 || true`);
   exec.execSync(`node ./node_modules/react-native/local-cli/cli.js start --root=./playground`);
 }
-
-// function findPort() {
-//   let port = 8081;
-//   if (!process.env.CI) {
-//     return port;
-//   }
-//   while (!isPortOpen(port)) {
-//     port = Number((Math.random() * 1000).toFixed(0)) + 35000;
-//   }
-//   return port;
-// }
-
-// function isPortOpen(port) {
-//   try {
-//     return !exec.execSyncRead(`netstat -vanp tcp | grep ${port}`);
-//   } catch (e) {
-//     return true;
-//   }
-// }
