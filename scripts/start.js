255713dc72f6064403cbb6d24d1a52c9200a31c1
Daniel Zlotin <danielzl@wix.com>
2017-03-29 11:54:30 +0300

    try fix travis

--- scripts/start.js
+++ scripts/start.js
@@ -3,27 +3,27 @@ const exec = require('shell-utils').exec;
 run();
 
 function run() {
-  const port = findPort();
+  exec.kill('8081');
   exec.execSync(`watchman watch-del-all || true`);
-  exec.execSync(`adb reverse tcp:${port} tcp:${port} || true`);
-  exec.execSync(`node ./node_modules/react-native/local-cli/cli.js start --port=${port} --root=./playground`);
+  exec.execSync(`adb reverse tcp:8081 tcp:8081 || true`);
+  exec.execSync(`node ./node_modules/react-native/local-cli/cli.js start --root=./playground`);
 }
 
-function findPort() {
-  let port = 8081;
-  if (!process.env.CI) {
-    return port;
-  }
-  while (!isPortOpen(port)) {
-    port = Number((Math.random() * 1000).toFixed(0)) + 35000;
-  }
-  return port;
-}
+// function findPort() {
+//   let port = 8081;
+//   if (!process.env.CI) {
+//     return port;
+//   }
+//   while (!isPortOpen(port)) {
+//     port = Number((Math.random() * 1000).toFixed(0)) + 35000;
+//   }
+//   return port;
+// }
 
-function isPortOpen(port) {
-  try {
-    return !exec.execSyncRead(`netstat -vanp tcp | grep ${port}`);
-  } catch (e) {
-    return true;
-  }
-}
+// function isPortOpen(port) {
+//   try {
+//     return !exec.execSyncRead(`netstat -vanp tcp | grep ${port}`);
+//   } catch (e) {
+//     return true;
+//   }
+// }
