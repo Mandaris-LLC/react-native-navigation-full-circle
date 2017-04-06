076e08ec106535c6dc45e635233665dd4024453c
Daniel Zlotin <danielzl@wix.com>
2017-04-06 18:10:08 +0300

    clean script

---
+++ scripts/clean.js
@@ -0,0 +1,13 @@
+const exec = require('shell-utils').exec;
+
+run();
+
+function run() {
+  exec.kill('8081');
+  exec.execSync(`watchman watch-del-all || true`);
+  exec.execSync(`adb reverse tcp:8081 tcp:8081 || true`);
+  exec.execSync(`rm -rf lib/ios/DerivedData/`);
+  exec.execSync(`rm -rf playground/ios/DerivedData/`);
+  exec.execSync(`cd lib/android && ./gradlew clean`);
+  exec.execSync(`cd playground/android && ./gradlew clean`);
+}
