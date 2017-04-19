754b1d5302cc39aac98ed76554ee9ff0540dcb5a
Daniel Zlotin <danielzl@wix.com>
2017-04-19 16:45:00 +0300

    update version to 1.1, make master latest

--- scripts/release.js
+++ scripts/release.js
@@ -3,15 +3,15 @@ const p = require('path');
 const semver = require('semver');
 
 function execSync(cmd) {
-  cp.execSync(cmd, {stdio: ['inherit', 'inherit', 'inherit']});
+  cp.execSync(cmd, { stdio: ['inherit', 'inherit', 'inherit'] });
 }
 
 function execSyncRead(cmd) {
-  return String(cp.execSync(cmd, {stdio: ['inherit', 'pipe', 'inherit']})).trim();
+  return String(cp.execSync(cmd, { stdio: ['inherit', 'pipe', 'inherit'] })).trim();
 }
 
 function execSyncSilently(cmd) {
-  cp.execSync(cmd, {stdio: ['ignore', 'ignore', 'ignore']});
+  cp.execSync(cmd, { stdio: ['ignore', 'ignore', 'ignore'] });
 }
 
 function validateEnv() {
@@ -42,9 +42,14 @@ function setupGit() {
 }
 
 function calcNewVersion() {
-  const nextTaggedVersion = execSyncRead(`npm view ${process.env.npm_package_name}@next version`);
-  console.log(`next tagged version is: ${nextTaggedVersion}`);
-  return semver.inc(nextTaggedVersion, 'prerelease');
+  const latestVersion = execSyncRead(`npm view ${process.env.npm_package_name}@latest version`);
+  console.log(`latest version is: ${latestVersion}`);
+  console.log(`package version is: ${process.env.npm_package_version}`);
+  if (semver.gt(process.env.npm_package_version, latestVersion)) {
+    return semver.inc(process.env.npm_package_version, 'patch');
+  } else {
+    return semver.inc(latestVersion, 'patch');
+  }
 }
 
 function copyNpmRc() {
@@ -56,7 +61,7 @@ function tagAndPublish(newVersion) {
   console.log(`new version is: ${newVersion}`);
   execSync(`npm version ${newVersion} -m "v${newVersion} [ci skip]"`);
   execSyncSilently(`git push deploy --tags`);
-  execSync(`npm publish --tag next`);
+  execSync(`npm publish --tag latest`);
 }
 
 function run() {
