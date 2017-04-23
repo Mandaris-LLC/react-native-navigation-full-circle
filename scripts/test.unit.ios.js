f721116eb5d0efb0e38ec8b1dbdb9970b4493691
Daniel Zlotin <danielzl@wix.com>
2017-04-23 11:01:52 +0300

    try reproduce travis bug

--- scripts/test.unit.ios.js
+++ scripts/test.unit.ios.js
@@ -4,11 +4,11 @@ const exec = require('shell-utils').exec;
 const release = _.includes(process.argv, 'release');
 
 function runWithXcpretty(cmd) {
-  if (exec.which(`xcpretty`)) {
-    exec.execSync(`${cmd} | xcpretty && exit \${PIPESTATUS[0]}`);
-  } else {
-    exec.execSync(`${cmd}`);
-  }
+  // if (exec.which(`xcpretty`)) {
+  //   exec.execSync(`${cmd} | xcpretty && exit \${PIPESTATUS[0]}`);
+  // } else {
+  exec.execSync(`${cmd}`);
+  // }
 }
 
 function run() {
@@ -21,7 +21,7 @@ function run() {
             -sdk iphonesimulator
             -configuration ${conf}
             -derivedDataPath ./DerivedData/playground
-            ONLY_ACTIVE_ARCH=YES`);
+            ONLY_ACTIVE_ARCH=YES && echo $?`);
   runWithXcpretty(`RCT_NO_LAUNCH_PACKAGER=true
           cd ./playground/ios && xcodebuild
             test-without-building
@@ -31,7 +31,7 @@ function run() {
             -configuration ${conf}
             -destination 'platform=iOS Simulator,name=iPhone 7'
             -derivedDataPath ./DerivedData/playground
-            ONLY_ACTIVE_ARCH=YES`);
+            ONLY_ACTIVE_ARCH=YES && echo $?`);
 }
 
 run();
