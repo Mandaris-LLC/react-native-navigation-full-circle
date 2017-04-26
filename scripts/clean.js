2e74d2c8685a81ff62ce2538f5d07175c03e81ca
Daniel Zlotin <danielzl@wix.com>
2017-04-26 19:28:04 +0300

    try fix travis

--- scripts/clean.js
+++ scripts/clean.js
@@ -3,7 +3,7 @@ const exec = require('shell-utils').exec;
 run();
 
 function run() {
-  exec.kill('8081');
+  exec.execSync('lsof -t -i tcp:8081 | xargs kill || true');
   exec.execSync(`watchman watch-del-all || true`);
   exec.execSync(`adb reverse tcp:8081 tcp:8081 || true`);
   exec.execSync(`rm -rf lib/ios/DerivedData/`);
