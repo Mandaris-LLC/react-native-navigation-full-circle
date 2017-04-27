0786940ef8f211d630f91bb451a96bc538a4146e
Daniel Zlotin <danielzl@wix.com>
2017-04-27 18:47:29 +0300

    support for android test release builds, fixed #1154

--- scripts/test.e2e.android.js
+++ scripts/test.e2e.android.js
@@ -10,7 +10,7 @@ function run() {
   }
 
   exec.execSync(`yarn run uninstall-android`);
-  exec.execSync(`yarn run install-android`);
+  exec.execSync(`yarn run install-android ${release ? '-- release' : ''}`);
   exec.execSync(`cd AndroidE2E && ./gradlew connectedDebugAndroidTest`);
 }
 
