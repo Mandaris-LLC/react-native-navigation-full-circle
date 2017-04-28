b1f5cfefbb887bafc56982ee801257a3645b0ec2
Guy Carmeli <guyca@users.noreply.github.com>
2017-04-28 12:14:36 +0300

    Render children and pass props to children (#1159)
    Render children and pass props to children
    Export SharedElementTransition on iOS
    Omit children from passed props

--- src/views/sharedElementTransition.ios.js
+++ src/views/sharedElementTransition.ios.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 import {
-  requireNativeComponent,
   View
 } from 'react-native';
 
 export default class SharedElementTransition extends Component {
+  static propTypes = {
+    children: PropTypes.object
+  };
+
   render() {
-    return <View />;
+    const {children, ...restProps} = this.props;
+    return (
+      <View {...restProps}>
+        {children}
+      </View>
+    );
   }
 }
