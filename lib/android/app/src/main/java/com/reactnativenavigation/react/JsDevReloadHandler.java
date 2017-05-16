93d074adf7781268e1b55ef0e270ca08047ca6fc
Daniel Zlotin <danielzl@wix.com>
2017-05-16 13:46:27 +0300

    unneeded

--- lib/android/app/src/main/java/com/reactnativenavigation/react/JsDevReloadHandler.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/JsDevReloadHandler.java
@@ -3,7 +3,6 @@ package com.reactnativenavigation.react;
 import android.view.KeyEvent;
 
 import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.bridge.ReactContext;
 
 public class JsDevReloadHandler {
 	private final ReactInstanceManager reactInstanceManager;
@@ -11,12 +10,6 @@ public class JsDevReloadHandler {
 
 	public JsDevReloadHandler(final ReactInstanceManager reactInstanceManager) {
 		this.reactInstanceManager = reactInstanceManager;
-		reactInstanceManager.addReactInstanceEventListener(new ReactInstanceManager.ReactInstanceEventListener() {
-			@Override
-			public void onReactContextInitialized(final ReactContext context) {
-				//this is too late
-			}
-		});
 	}
 
 	public boolean onKeyUp(int keyCode) {
