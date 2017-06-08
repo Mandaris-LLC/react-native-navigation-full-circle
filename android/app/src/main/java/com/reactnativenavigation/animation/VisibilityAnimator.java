8fd1a231adfb726e9a14c36e5a5e1b6726321c77
Guy Carmeli <guyca@users.noreply.github.com>
2017-06-08 18:01:45 +0300

    Revert "Fix title bar not displayed if it was initially hidden (#1341)" (#1342)
    This reverts commit 7919e659321b562ee9372575e6bdce57f569ad7c.

--- android/app/src/main/java/com/reactnativenavigation/animation/VisibilityAnimator.java
+++ android/app/src/main/java/com/reactnativenavigation/animation/VisibilityAnimator.java
@@ -1,16 +1,19 @@
 package com.reactnativenavigation.animation;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.view.View;
 
 public class VisibilityAnimator {
 
-    private final LinearOutSlowInInterpolator interpolator = new LinearOutSlowInInterpolator();
-    private ObjectAnimator animator;
-
     public enum HideDirection {
         Up, Down
+    }
+
+    private enum VisibilityState {
+        Hidden, AnimateHide, Shown, AnimateShow
     }
 
     private static final int SHOW_END_VALUE = 0;
@@ -18,6 +21,7 @@ public class VisibilityAnimator {
 
     private final View view;
     private final int hiddenEndValue;
+    private VisibilityState visibilityState = VisibilityState.Shown;
 
     public VisibilityAnimator(View view, HideDirection hideDirection, int height) {
         this.view = view;
@@ -25,45 +29,56 @@ public class VisibilityAnimator {
     }
 
     public void setVisible(boolean visible, boolean animate) {
-        cancelAnimator();
-        if (visible) {
+        if (visible && isHiding()) {
             show(animate);
-        } else {
+        } else if (!visible && isShowing()) {
             hide(animate);
-        }
-    }
-
-    private void cancelAnimator() {
-        if (animator != null && animator.isRunning()) {
-            view.clearAnimation();
-            animator.cancel();
         }
     }
 
     private void show(boolean animate) {
         if (animate) {
-            animator = createAnimator(true);
+            ObjectAnimator animator = createAnimator(true);
             animator.start();
         } else {
-            view.setTranslationY(SHOW_END_VALUE);
-            view.setY(SHOW_END_VALUE);
+            visibilityState = VisibilityState.Shown;
+            view.setVisibility(View.VISIBLE);
         }
     }
 
     private void hide(boolean animate) {
         if (animate) {
-            animator = createAnimator(false);
+            ObjectAnimator animator = createAnimator(false);
             animator.start();
         } else {
-            view.setTranslationY(hiddenEndValue);
-            view.setY(hiddenEndValue);
+            visibilityState = VisibilityState.Hidden;
+            view.setVisibility(View.GONE);
         }
     }
 
+    private boolean isShowing() {
+        return visibilityState == VisibilityState.Shown || visibilityState == VisibilityState.AnimateShow;
+    }
+
+    private boolean isHiding() {
+        return visibilityState == VisibilityState.Hidden || visibilityState == VisibilityState.AnimateHide;
+    }
+
     private ObjectAnimator createAnimator(final boolean show) {
-        final ObjectAnimator animator = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, show ? SHOW_END_VALUE : hiddenEndValue);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, show ? SHOW_END_VALUE : hiddenEndValue);
         animator.setDuration(DURATION);
-        animator.setInterpolator(interpolator);
+        animator.setInterpolator(new LinearOutSlowInInterpolator());
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                visibilityState = show ? VisibilityState.AnimateShow : VisibilityState.AnimateHide;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                visibilityState = show ? VisibilityState.Shown : VisibilityState.Hidden;
+            }
+        });
         return animator;
     }
 }
--- android/app/src/main/java/com/reactnativenavigation/views/TitleBar.java
+++ android/app/src/main/java/com/reactnativenavigation/views/TitleBar.java
@@ -33,6 +33,11 @@ public class TitleBar extends Toolbar {
     }
 
     @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    @Override
     public void onViewAdded(View child) {
         super.onViewAdded(child);
         if (child instanceof ActionMenuView) {
@@ -71,6 +76,7 @@ public class TitleBar extends Toolbar {
     }
 
     public void setStyle(StyleParams params) {
+        setVisibility(params.titleBarHidden ? GONE : VISIBLE);
         setTitleTextColor(params);
         setTitleTextFont(params);
         setSubtitleTextColor(params);
