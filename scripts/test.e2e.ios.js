aef9f0de761b56311419ee27bc6628cf8a78a615
Daniel Zlotin <danielzl@wix.com>
2017-09-05 14:25:03 +0300

    Revert "remove fbsimctl"
    This reverts commit 082a65d7917e516e2309a2013b26381f58c3c553.

--- scripts/test.e2e.ios.js
+++ scripts/test.e2e.ios.js
@@ -34,6 +34,9 @@ function e2e() {
 }
 
 function run() {
+  if (!exec.which(`fbsimctl`)) {
+    throw new Error(`fbsimctl must be installed: "brew tap facebook/fb && brew install fbsimctl"`);
+  }
   buildProjForDetox();
   e2e();
 }
