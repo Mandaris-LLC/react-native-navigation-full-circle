5c266e4436de130b22621072d7257f89600e80f4
Guy Carmeli <guyc@wix.com>
2017-09-29 13:24:26 +0300

    Finish SplahActivity if react context exists
    Fixes #1938 Closes #1900

--- android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/controllers/SplashActivity.java
@@ -27,6 +27,10 @@ public abstract class SplashActivity extends AppCompatActivity {
 
         if (NavigationApplication.instance.getReactGateway().hasStartedCreatingContext()) {
             NavigationApplication.instance.getEventEmitter().sendAppLaunchedEvent();
+            if (NavigationApplication.instance.clearHostOnActivityDestroy()) {
+                overridePendingTransition(0, 0);
+                finish();
+            }
             return;
         }
 
