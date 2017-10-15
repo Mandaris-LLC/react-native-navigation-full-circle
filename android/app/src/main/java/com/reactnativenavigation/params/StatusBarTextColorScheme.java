2ce161d09e235b8231365117bd2a80e9bd41811e
Guy Carmeli <guyc@wix.com>
2017-10-15 10:31:43 +0300

    default to statusBarTextColorScheme from AppStyle

--- android/app/src/main/java/com/reactnativenavigation/params/StatusBarTextColorScheme.java
+++ android/app/src/main/java/com/reactnativenavigation/params/StatusBarTextColorScheme.java
@@ -6,15 +6,15 @@ import android.support.annotation.Nullable;
 public enum StatusBarTextColorScheme {
     Light, Dark, Undefined;
 
-    public static StatusBarTextColorScheme fromString(@Nullable String colorScheme) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || colorScheme == null) return Undefined;
+    public static StatusBarTextColorScheme fromString(@Nullable String colorScheme, StatusBarTextColorScheme defaultScheme) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || colorScheme == null) return defaultScheme;
         switch (colorScheme) {
             case "light":
                 return Light;
             case "dark":
                 return Dark;
             default:
-                return Undefined;
+                return defaultScheme;
         }
     }
 }
--- android/app/src/main/java/com/reactnativenavigation/params/parsers/StyleParamsParser.java
+++ android/app/src/main/java/com/reactnativenavigation/params/parsers/StyleParamsParser.java
@@ -30,7 +30,7 @@ public class StyleParamsParser {
         result.orientation = Orientation.fromString(params.getString("orientation", getDefaultOrientation()));
         result.statusBarColor = getColor("statusBarColor", getDefaultStatusBarColor());
         result.statusBarHidden = getBoolean("statusBarHidden", getDefaultStatusHidden());
-        result.statusBarTextColorScheme = StatusBarTextColorScheme.fromString(params.getString("statusBarTextColorScheme"));
+        result.statusBarTextColorScheme = StatusBarTextColorScheme.fromString(params.getString("statusBarTextColorScheme"), getDefaultStatusBarTextColorScheme());
         result.contextualMenuStatusBarColor = getColor("contextualMenuStatusBarColor", getDefaultContextualMenuStatusBarColor());
         result.contextualMenuButtonsColor = getColor("contextualMenuButtonsColor", getDefaultContextualMenuButtonsColor());
         result.contextualMenuBackgroundColor = getColor("contextualMenuBackgroundColor", getDefaultContextualMenuBackgroundColor());
@@ -97,6 +97,10 @@ public class StyleParamsParser {
         result.bottomTabFontFamily = getFont("bottomTabFontFamily", getDefaultBottomTabsFontFamily());
 
         return result;
+    }
+
+    private StatusBarTextColorScheme getDefaultStatusBarTextColorScheme() {
+        return AppStyle.appStyle == null ? StatusBarTextColorScheme.Undefined : AppStyle.appStyle.statusBarTextColorScheme;
     }
 
     private String getDefaultOrientation() {
