f1428556f847f98b859b3852f174e828b32df342
Daniel Zlotin <danielzl@wix.com>
2017-11-23 13:22:48 +0200

    matrix 3 way

--- scripts/travis.android.sh
+++ scripts/travis.android.sh
@@ -1,5 +1,4 @@
 ./scripts/travis-fold.sh "npm install"
 ./scripts/travis-fold.sh "npm run clean"
-./scripts/travis-fold.sh "npm run test-js"
 ./scripts/travis-fold.sh "npm run test-unit-android -- --release"
 ./scripts/travis-fold.sh "npm run test-e2e-android -- --release"
