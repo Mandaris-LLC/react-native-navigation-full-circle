b0df3fd6d4e19bd4e1d0fe0d174d3f370a418e30
Daniel Zlotin <danielzl@wix.com>
2017-11-23 11:38:44 +0200

    try fix travis

--- scripts/env/installNode.sh
+++ scripts/env/installNode.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install stable
