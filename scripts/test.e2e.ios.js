14fa88a623db16f3c5da4352d1fee48c701ca96b
Daniel Zlotin <danielzl@wix.com>
2017-12-17 19:05:54 +0200

    another try

--- scripts/test.e2e.ios.js
+++ scripts/test.e2e.ios.js
@@ -12,7 +12,6 @@ function run() {
     exec.execAsync(`sleep 10`).then(() => {
       exec.execAsync(`open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app`);
     });
-
     startRecording();
     exec.execSync(`detox test --configuration ios.sim.${conf} ${process.env.CI ? '--cleanup' : ''}`);
   } finally {
