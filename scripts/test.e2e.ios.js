459cb003d931259f2ade192130ee9ce15adf9fd3
Daniel Zlotin <danielzl@wix.com>
2017-12-17 14:32:37 +0200

    try fix travis by recording and uploading video

--- scripts/test.e2e.ios.js
+++ scripts/test.e2e.ios.js
@@ -6,7 +6,23 @@ const release = _.includes(process.argv, '--release');
 run();
 
 function run() {
-  const conf = release ? `release` : `debug`;
-  exec.execSync(`detox build --configuration ios.sim.${conf} && detox test --configuration ios.sim.${conf} ${process.env.CI ? '--cleanup' : ''}`);
+  try {
+    startRecording();
+    const conf = release ? `release` : `debug`;
+    exec.execSync(`detox build --configuration ios.sim.${conf} && detox test --configuration ios.sim.${conf} ${process.env.CI ? '--cleanup' : ''}`);
+  } catch (err) {
+    stopRecording();
+  }
 }
 
+function startRecording() {
+  const json = JSON.parse(exec.execSyncRead(`applesimutils --list --byName "iPhone SE"`));
+  const deviceId = json[0].udid;
+  exec.execAsync(`xcrun simctl bootstatus ${deviceId} && xcrun simctl io booted recordVideo --type=mp4 video.mp4`);
+}
+
+function stopRecording() {
+  const pid = exec.execSyncRead(`pgrep simctl`);
+  exec.execSync(`kill -sigint ${pid}`);
+  exec.execSync(`npm run release`);
+}
