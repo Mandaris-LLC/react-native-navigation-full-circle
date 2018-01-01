8cb407c7698a1da5724b097b3d3ed77f5ac242bd
yogevbd <yogevbd@wix.com>
2018-01-01 16:08:13 +0200

    fixes #2353

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -405,6 +405,7 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
         [mergedAttributes addEntriesFromDictionary:previousAttributes];
 
         [item setTitleTextAttributes:[mergedAttributes copy] forState:UIControlStateNormal];
+        [item setTitleTextAttributes:[mergedAttributes copy] forState:UIControlStateHighlighted];
       }
     }
 
@@ -421,11 +422,13 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
       [mergedAttributes addEntriesFromDictionary:previousAttributes];
 
       [item setTitleTextAttributes:[mergedAttributes copy] forState:UIControlStateNormal];
+      [item setTitleTextAttributes:[mergedAttributes copy] forState:UIControlStateHighlighted];
     }
 
     // At the moment, this seems to be the only thing that gets the back button correctly
     [navButtonTextAttributes removeObjectForKey:NSForegroundColorAttributeName];
     [[UIBarButtonItem appearance] setTitleTextAttributes:navButtonTextAttributes forState:UIControlStateNormal];
+    [[UIBarButtonItem appearance] setTitleTextAttributes:navButtonTextAttributes forState:UIControlStateHighlighted];
   }
   
   NSString *navBarButtonColor = self.navigatorStyle[@"navBarButtonColor"];
