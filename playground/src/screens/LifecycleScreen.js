c461d05f6f8d0f82753d04e134c3224a0f97d22c
Daniel Zlotin <danielzl@wix.com>
2018-01-10 12:08:15 +0200

    fix tests

--- playground/src/screens/LifecycleScreen.js
+++ playground/src/screens/LifecycleScreen.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const { Component } = require('react');
 
-const { View, Text, Button, Platform } = require('react-native');
+const { View, Text, Button } = require('react-native');
 
 const Navigation = require('react-native-navigation');
 const testIDs = require('../testIDs');
