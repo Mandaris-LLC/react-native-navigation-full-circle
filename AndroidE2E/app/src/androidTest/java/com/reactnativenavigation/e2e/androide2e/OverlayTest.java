21ded978bb03334621933ed179d595ee9445ff5b
Andres Aguilar <andresesfm@gmail.com>
2018-01-25 12:05:45 -0800

    fixed failing unit test

--- AndroidE2E/app/src/androidTest/java/com/reactnativenavigation/e2e/androide2e/OverlayTest.java
+++ AndroidE2E/app/src/androidTest/java/com/reactnativenavigation/e2e/androide2e/OverlayTest.java
@@ -7,9 +7,19 @@ import org.junit.Test;
 public class OverlayTest extends BaseTest {
 
 	@Test
-	public void testOverlayAlertAppear() throws Exception {
+	public void testOverlayInterAlertAppear() throws Exception {
 		elementByText("PUSH OPTIONS SCREEN").click();
-        elementByText("SHOW OVERLAY").click();
+        elementByText("SHOW OVERLAY INTER").click();
+		assertExists(By.text("Test view"));
+		elementByText("OK").click();
+		assertExists(By.text("Overlay disappeared"));
+        elementByText("OK").click();
+	}
+
+	@Test
+	public void testOverlayNotInterAlertAppear() throws Exception {
+		elementByText("PUSH OPTIONS SCREEN").click();
+        elementByText("SHOW TOUCH THROUGH OVERLAY NOTINTER").click();
 		assertExists(By.text("Test view"));
 		elementByText("OK").click();
 		assertExists(By.text("Overlay disappeared"));
