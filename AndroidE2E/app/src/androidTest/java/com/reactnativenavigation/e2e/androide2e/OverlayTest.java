b541d796b565b6214cc868b785112909e49adc42
Guy Carmeli <guyca@users.noreply.github.com>
2018-01-26 10:28:19 +0200

    Test not intercepting overlay (#2598)

--- AndroidE2E/app/src/androidTest/java/com/reactnativenavigation/e2e/androide2e/OverlayTest.java
+++ AndroidE2E/app/src/androidTest/java/com/reactnativenavigation/e2e/androide2e/OverlayTest.java
@@ -1,28 +1,33 @@
 package com.reactnativenavigation.e2e.androide2e;
 
 import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObjectNotFoundException;
 
 import org.junit.Test;
 
 public class OverlayTest extends BaseTest {
 
 	@Test
-	public void testOverlayInterAlertAppear() throws Exception {
+	public void testOverlayAlertAppear() throws Exception {
 		elementByText("PUSH OPTIONS SCREEN").click();
-        elementByText("SHOW OVERLAY INTER").click();
+        elementByText("SHOW OVERLAY").click();
 		assertExists(By.text("Test view"));
-		elementByText("OK").click();
-		assertExists(By.text("Overlay disappeared"));
-        elementByText("OK").click();
+        assetDismissed();
 	}
 
-	@Test
-	public void testOverlayNotInterAlertAppear() throws Exception {
+    @Test
+	public void testOverlayNotInterceptingTouchEvents() throws Exception {
 		elementByText("PUSH OPTIONS SCREEN").click();
-        elementByText("SHOW TOUCH THROUGH OVERLAY NOTINTER").click();
+        elementByText("SHOW TOUCH THROUGH OVERLAY").click();
 		assertExists(By.text("Test view"));
-		elementByText("OK").click();
-		assertExists(By.text("Overlay disappeared"));
-        elementByText("OK").click();
+        elementByText("DYNAMIC OPTIONS").click();
+        assertExists(By.text("Dynamic Title"));
+        assetDismissed();
 	}
+
+    private void assetDismissed() throws UiObjectNotFoundException {
+        elementByText("OK").click();
+        assertExists(By.text("Overlay disappeared"));
+        elementByText("OK").click();
+    }
 }
