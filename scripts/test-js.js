0c8e928adf907a587a7bdc67a50bba32c9a613fb
Daniel Zlotin <danielzl@wix.com>
2018-01-29 14:48:02 +0200

    assert all ts files

--- scripts/test-js.js
+++ scripts/test-js.js
@@ -16,5 +16,15 @@ run();
 function run() {
   const paths = _.chain(dirs).map((d) => `'${d}/**/*.[tj]s*'`).join(' ').value();
   exec.execSync(`tslint ${paths} ${fix} --format verbose`);
+  assertAllTsFilesInSrc();
   exec.execSync(`jest --coverage`);
+}
+
+function assertAllTsFilesInSrc() {
+  const allFiles = exec.execSyncRead('find ./lib/src -type f');
+  const lines = _.split(allFiles, '\n');
+  const offenders = _.filter(lines, (f) => !f.endsWith('.ts') && !f.endsWith('.tsx'));
+  if (offenders.length) {
+    throw new Error(`\n\nOnly ts/tsx files are allowed:\n${offenders.join('\n')}\n\n\n`);
+  }
 }
