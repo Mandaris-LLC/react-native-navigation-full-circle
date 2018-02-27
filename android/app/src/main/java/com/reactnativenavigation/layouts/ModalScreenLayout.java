9534fe9402206a0ce9473b71a4abf1bfbff8994f
Guy Carmeli <guyc@wix.com>
2018-02-27 19:48:18 +0200

    Fix flickering modal on Android
    The initial modal screen got animated twice, this regression was introduced in #2761.

--- android/app/src/main/java/com/reactnativenavigation/layouts/ModalScreenLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/ModalScreenLayout.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import com.reactnativenavigation.params.ScreenParams;
 import com.reactnativenavigation.views.LeftButtonOnClickListener;
 
+import java.util.List;
+
 public class ModalScreenLayout extends SingleScreenLayout {
 
     public ModalScreenLayout(AppCompatActivity activity,
@@ -16,6 +18,23 @@ public class ModalScreenLayout extends SingleScreenLayout {
 
     @Override
     protected void pushInitialScreen(LayoutParams lp) {
-        stack.pushInitialModalScreenWithAnimation(screenParams, lp);
+        if (screenParams.screens.isEmpty()) {
+            stack.pushInitialModalScreenWithAnimation(screenParams, lp);
+        } else {
+            stack.pushInitialScreen(screenParams, lp);
+        }
+    }
+
+    @Override
+    protected void pushAdditionalScreens(LayoutParams lp) {
+        List<ScreenParams> screens = screenParams.screens;
+        for (int i = 0, screensSize = screens.size(); i < screensSize; i++) {
+            ScreenParams screen = screens.get(i);
+            if (i == screens.size() - 1) {
+                stack.pushInitialModalScreenWithAnimation(screen, lp);
+            } else {
+                stack.pushInitialScreen(screen, lp);
+            }
+        }
     }
 }
--- android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
+++ android/app/src/main/java/com/reactnativenavigation/layouts/SingleScreenLayout.java
@@ -86,17 +86,17 @@ public class SingleScreenLayout extends BaseLayout {
         LayoutParams lp = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
         pushInitialScreen(lp);
         pushAdditionalScreens(lp);
-        stack.show(NavigationType.Push);
     }
 
     protected void pushInitialScreen(LayoutParams lp) {
         stack.pushInitialScreen(screenParams, lp);
     }
 
-    private void pushAdditionalScreens(LayoutParams lp) {
+    protected void pushAdditionalScreens(LayoutParams lp) {
         for (ScreenParams screen : screenParams.screens) {
             stack.pushInitialScreen(screen, lp);
         }
+        stack.show(NavigationType.Push);
     }
 
     private void sendScreenChangedEventAfterInitialPush() {
