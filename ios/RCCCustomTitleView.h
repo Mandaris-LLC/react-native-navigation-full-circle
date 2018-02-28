2c0ec109a8f5a167914e0c490f6bebb04a860541
yogevbd <yogevbd@wix.com>
2018-02-28 16:27:23 +0200

    Closes #2777

--- ios/RCCCustomTitleView.h
+++ ios/RCCCustomTitleView.h
@@ -7,10 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <React/RCTRootView.h>
+#import <React/RCTRootViewDelegate.h>
 
-@interface RCCCustomTitleView : UIView
+@interface RCCCustomTitleView : UIView <RCTRootViewDelegate>
 
--(instancetype)initWithFrame:(CGRect)frame subView:(UIView*)subView alignment:(NSString*)alignment;
-- (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator;
+- (instancetype)initWithFrame:(CGRect)frame subView:(RCTRootView*)subView alignment:(NSString*)alignment;
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator;
 
 @end
--- ios/RCCCustomTitleView.m
+++ ios/RCCCustomTitleView.m
@@ -9,101 +9,55 @@
 #import "RCCCustomTitleView.h"
 
 @interface RCCCustomTitleView ()
-@property (nonatomic, strong) UIView *subView;
-@property (nonatomic, strong) NSString *subViewAlign;
-@property float initialWidth;
+
+@property (nonatomic, strong) RCTRootView *subView;
+@property (nonatomic, strong) NSString *alignment;
+
 @end
 
 @implementation RCCCustomTitleView
 
 
--(instancetype)initWithFrame:(CGRect)frame subView:(UIView*)subView alignment:(NSString*)alignment {
-    _initialWidth = frame.size.width;
-    self = [super initWithFrame:frame];
+- (instancetype)initWithFrame:(CGRect)frame subView:(RCTRootView*)subView alignment:(NSString*)alignment {
+    self = [super init];
     
     if (self) {
-        self.backgroundColor = [UIColor clearColor];
         self.subView = subView;
-        self.subViewAlign = alignment;
+        self.alignment = alignment;
         
-        subView.frame = self.bounds;
+        self.backgroundColor = [UIColor clearColor];
+        self.subView.backgroundColor = [UIColor clearColor];
+        
+        if ([alignment isEqualToString:@"fill"]) {
+            self.frame = frame;
+            self.subView.frame = self.frame;
+        } else {
+            self.subView.delegate = self;
+        }
+        
         [self addSubview:subView];
     }
     
     return self;
 }
 
-
--(void)layoutSubviews {
-    [super layoutSubviews];
-    
-    if ([self.subViewAlign isEqualToString:@"fill"]) {
-        self.subView.frame = self.bounds;
-    }
-    else {
-        
-        CGFloat superViewWidth = self.superview.frame.size.width;
-        CGFloat paddingLeftFromCenter = (superViewWidth/2) - self.frame.origin.x;
-        CGFloat paddingRightFromCenter = self.frame.size.width - paddingLeftFromCenter;;
-        CGRect reactViewFrame = self.subView.bounds;
-        CGFloat minPadding = MIN(paddingLeftFromCenter, paddingRightFromCenter);
-        
-        reactViewFrame.size.width = minPadding*2;
-        reactViewFrame.origin.x = paddingLeftFromCenter - minPadding;
-        self.subView.frame = reactViewFrame;
+- (void)rootViewDidChangeIntrinsicSize:(RCTRootView *)rootView {
+    if ([self.alignment isEqualToString:@"center"]) {
+        [self setFrame:CGRectMake(0, 0, rootView.intrinsicContentSize.width, rootView.intrinsicContentSize.height)];
+        [self.subView setFrame:CGRectMake(0, 0, rootView.intrinsicContentSize.width, rootView.intrinsicContentSize.height)];
     }
 }
-
-- (void)setFrame:(CGRect) frame {
-    float referenceWidth = [self statusBarWidth];
-    if (referenceWidth == 0) {
-        referenceWidth = _initialWidth;
-    }
-    float newNavBarWidth = frame.size.width;
-    BOOL frameNeedsToBeCorrected = newNavBarWidth < referenceWidth || CGRectEqualToRect(self.frame, CGRectZero);
-
-    if (frameNeedsToBeCorrected) {
-        // first we need to find out the total point diff of the status bar and the nav bar
-        float navBarHorizontalMargin = referenceWidth - newNavBarWidth;
-        
-        CGRect correctedFrame = frame;
-
-        // then we need to place the nav bar half times the horizontal margin to the left
-        correctedFrame.origin.x = -(navBarHorizontalMargin / 2);
-        
-        // and finally set the width so that it's equal to the status bar width
-        correctedFrame.size.width = referenceWidth;
-        
-        [super setFrame:correctedFrame];
-    } else if (frame.size.height != self.frame.size.height) { // otherwise
-        // if only the height has changed
-        CGRect newHeightFrame = self.frame;
-        // make sure we update just the height
-        newHeightFrame.size.height = frame.size.height;
-        [super setFrame:newHeightFrame];
-    }
-    
-    // keep a ref to the last frame, so that we avoid setting the frame twice for no reason
-//    _lastFrame = frame;
-}
-
 
 - (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
     // whenever the orientation changes this runs
     // and sets the nav bar item width to the new size width
     CGRect newFrame = self.frame;
-
+    
     if (newFrame.size.width < size.width) {
         newFrame.size.width = size.width;
         newFrame.origin.x = 0;
     }
     [super setFrame:newFrame];
 }
-
--(float) statusBarWidth {
-    CGSize statusBarSize = [[UIApplication sharedApplication] statusBarFrame].size;
-    return MAX(statusBarSize.width, statusBarSize.height);
-}
-
 
 @end
