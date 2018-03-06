067906fdb0df0dad4ece87818a5670fad8233f3d
Guy Carmeli <guyc@wix.com>
2018-03-06 17:41:49 +0200

    Fix static lifecycle events e2e on iOS
    Now iOS e2e is legitimately broken :)

--- e2e/StaticLifecycleEvents.test.js
+++ e2e/StaticLifecycleEvents.test.js
@@ -9,7 +9,7 @@ describe('Overlay', () => {
 
   it.skip('didAppear didDisappear', async () => {
     await elementById(testIDs.PUSH_STATIC_LIFECYCLE_BUTTON).tap();
-    await expect(elementByLabel('Static Lifecycle Events')).toBeVisible();
+    await expect(elementByLabel('Static Lifecycle Events Overlay')).toBeVisible();
     await expect(elementByLabel('didAppear | navigation.playground.StaticLifecycleOverlay')).toBeVisible();
     await elementByLabel('PUSH').tap();
     await expect(elementByLabel('didAppear | navigation.playground.PushedScreen')).toBeVisible();
