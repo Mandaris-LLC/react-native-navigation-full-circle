f12abaca4b3ea981d58d0931724b2b1f81012c8a
yogevbd <yogevbd@wix.com>
2018-03-26 10:37:23 +0300

    title bug fix on ios

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -619,9 +619,9 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
         }
     }
     
-    [self processTitleView:viewController
-                     props:self.navigatorStyle
-                     style:self.navigatorStyle];
+    // [self processTitleView:viewController
+    //                  props:self.navigatorStyle
+    //                  style:self.navigatorStyle];
     
 #if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_10_3
     if (@available(iOS 11.0, *)) {
