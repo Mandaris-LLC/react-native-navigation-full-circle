22f743bf5fe2f2443e92a8d3c6a098307f800564
Eli Perkins <eli.j.perkins@gmail.com>
2018-04-15 10:52:38 -0400

    Fix when a title image has nil or NSNull (#3033)
    This was occuring if a title has no properties set initially, but then are configured later. The title image data would go from [NSNull null] to nil, breaking this check.
    
    By comparing against both values, we can ensure the correct logic for all behaviors.

--- ios/Helpers/RCCTitleViewHelper.m
+++ ios/Helpers/RCCTitleViewHelper.m
@@ -115,7 +115,10 @@ navigationController:(UINavigationController*)navigationController
 
 -(BOOL)isTitleOnly
 {
-    return self.title && !self.subtitle && !self.titleImageData;
+    BOOL hasNoImageData = [[NSNull null] isEqual:self.titleImageData] || self.titleImageData == nil;
+    return self.title != nil &&
+        self.subtitle == nil &&
+        hasNoImageData;
 }
 
 
