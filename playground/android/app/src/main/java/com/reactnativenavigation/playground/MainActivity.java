a7391bb664d1f93e8c39ab54744656e3aa42e696
Guy Carmeli <guyc@wix.com>
2018-05-29 10:47:07 +0300

    Dismiss system alerts in playground app
    Hopefully, this will prevent Detox from timing out when a system alert pops up on the screen.

--- playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
+++ playground/android/app/src/main/java/com/reactnativenavigation/playground/MainActivity.java
@@ -1,6 +1,18 @@
 package com.reactnativenavigation.playground;
 
-import com.reactnativenavigation.*;
+import android.content.Intent;
+
+import com.reactnativenavigation.NavigationActivity;
 
 public class MainActivity extends NavigationActivity {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        dismissSystemAlertsToPreventDetoxFromTimingOut(hasFocus);
+    }
+
+    private void dismissSystemAlertsToPreventDetoxFromTimingOut(boolean hasFocus) {
+        if (! hasFocus) {
+            sendBroadcast(new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS));
+        }
+    }
 }
