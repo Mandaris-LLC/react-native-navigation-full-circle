7c53ac68c302a2d46700a1a892a2bd40a10bac1f
Guy Carmeli <guyc@wix.com>
2018-06-18 18:33:15 +0300

    Rename backButton id constant to backButtonId

--- lib/android/app/src/main/java/com/reactnativenavigation/react/Constants.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/react/Constants.java
@@ -1,7 +1,7 @@
 package com.reactnativenavigation.react;
 
 public class Constants {
-    public static final String BACK_BUTTON_JS_KEY = "backButton";
+    public static final String BACK_BUTTON_JS_KEY = "backButtonId";
     public static final String BACK_BUTTON_ID = "RNN.back";
     public static final String STATUS_BAR_HEIGHT_KEY = "statusBarHeight";
 }
