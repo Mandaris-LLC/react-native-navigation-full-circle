972bba2c9152dd803503644a6050295336536a5e
yogevbd <yogevbd@wix.com>
2018-07-03 17:52:46 +0300

    Closes #3484

--- lib/ios/UIImage+tint.m
+++ lib/ios/UIImage+tint.m
@@ -3,27 +3,14 @@
 @implementation UIImage (tint)
 
 - (UIImage *)withTintColor:(UIColor *)color {
-	UIGraphicsBeginImageContext(self.size);
-	CGContextRef context = UIGraphicsGetCurrentContext();
-	
-	CGContextTranslateCTM(context, 0, self.size.height);
-	CGContextScaleCTM(context, 1.0, -1.0);
-	
-	CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
-	
-	// draw alpha-mask
-	CGContextSetBlendMode(context, kCGBlendModeNormal);
-	CGContextDrawImage(context, rect, self.CGImage);
-	
-	// draw tint color, preserving alpha values of original image
-	CGContextSetBlendMode(context, kCGBlendModeSourceIn);
-	[color setFill];
-	CGContextFillRect(context, rect);
-	
-	UIImage *coloredImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIImage *newImage = [self imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+	UIGraphicsBeginImageContextWithOptions(self.size, NO, newImage.scale);
+	[color set];
+	[newImage drawInRect:CGRectMake(0, 0, self.size.width, newImage.size.height)];
+	newImage = UIGraphicsGetImageFromCurrentImageContext();
 	UIGraphicsEndImageContext();
 	
-	return coloredImage;
+	return newImage;
 }
 
 @end
