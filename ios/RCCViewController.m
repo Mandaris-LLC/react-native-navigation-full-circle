734e241c694d618c532b2637b42369eddd4e0f5c
NaoyaKurahashi <naoya.kurahashi@bizreach.co.jp>
2018-07-22 22:16:53 +0900

    revert screen change event condition and global scree event condition before 1.1.464 (#3548)

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -205,8 +205,8 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
 }
 
 - (void)sendScreenChangedEvent:(NSString *)eventName {
-    if (self.view != nil) {
-        RCTRootView *rootView = self.view;
+    if ([self.view isKindOfClass:[RCTRootView class]]) {
+        RCTRootView *rootView = (RCTRootView *)self.view;
         
         if (rootView.appProperties && rootView.appProperties[@"navigatorEventID"]) {
             
@@ -220,10 +220,9 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
 }
 
 - (void)sendGlobalScreenEvent:(NSString *)eventName endTimestampString:(NSString *)endTimestampStr shouldReset:(BOOL)shouldReset {
-    if (self.view != nil){
-        RCTRootView *rootView = self.view;
-        NSString *screenName = [rootView moduleName];
-        
+    if ([self.view isKindOfClass:[RCTRootView class]]){
+        NSString *screenName = [((RCTRootView *)self.view) moduleName];
+
         [[[RCCManager sharedInstance] getBridge].eventDispatcher sendAppEventWithName:eventName body:@
          {
              @"commandType": self.commandType ? self.commandType : @"",
