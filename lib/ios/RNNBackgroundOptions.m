850e98cd764840a03577d5c55f3c13763b97554b
yogevbd <yogevbd@wix.com>
2018-08-13 12:09:40 +0300

    support null background.color

--- lib/ios/RNNBackgroundOptions.m
+++ lib/ios/RNNBackgroundOptions.m
@@ -3,11 +3,9 @@
 @implementation RNNBackgroundOptions
 
 - (void)applyOn:(UIViewController *)viewController {
-	if (self.color) {
+	if (self.color && ![self.color isKindOfClass:[NSNull class]]) {
 		UIColor* backgroundColor = [RCTConvert UIColor:self.color];
 		viewController.navigationController.navigationBar.barTintColor = backgroundColor;
-	} else {
-		viewController.navigationController.navigationBar.barTintColor = nil;
 	}
 }
 
