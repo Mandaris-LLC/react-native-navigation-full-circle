9f32a721ee065f0a031e766c58a71b74976140f7
Guy Carmeli <guyc@wix.com>
2018-09-27 15:10:31 +0300

    Enable external component e2e on Android

--- e2e/ComplexLayout.test.js
+++ e2e/ComplexLayout.test.js
@@ -8,13 +8,13 @@ describe('complex layout', () => {
     await device.relaunchApp();
   });
 
-  test(':ios: shows external component in stack in modal', async () => {
+  test('shows external component in stack in modal', async () => {
     await elementById(testIDs.COMPLEX_LAYOUT_BUTTON).tap();
     await elementById(testIDs.EXTERNAL_COMPONENT_IN_STACK).tap();
     await expect(elementByLabel('External component in stack')).toBeVisible();
   });
 
-  test(':ios: shows external component in deep stack in modal', async () => {
+  test('shows external component in deep stack in modal', async () => {
     await elementById(testIDs.COMPLEX_LAYOUT_BUTTON).tap();
     await elementById(testIDs.EXTERNAL_COMPONENT_IN_DEEP_STACK).tap();
     await expect(elementByLabel('External component in deep stack')).toBeVisible();
