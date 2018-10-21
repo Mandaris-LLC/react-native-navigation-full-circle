9e6976d962dc0294c1a04c09e1637c5f617a7a01
yogevbd <yogevbd@wix.com>
2018-10-21 17:38:59 +0300

    Added tests for drawBehind

--- lib/ios/ReactNativeNavigationTests/UIViewController+RNNOptionsTest.m
+++ lib/ios/ReactNativeNavigationTests/UIViewController+RNNOptionsTest.m
@@ -1,9 +1,10 @@
 #import <XCTest/XCTest.h>
 #import "UIViewController+RNNOptions.h"
+#import <OCMock/OCMock.h>
 
 @interface UIViewController_RNNOptionsTest : XCTestCase
 
-@property (nonatomic, retain) UIViewController* uut;
+@property (nonatomic, retain) id uut;
 
 @end
 
@@ -11,27 +12,65 @@
 
 - (void)setUp {
     [super setUp];
-	self.uut = [UIViewController new];
+	self.uut = [OCMockObject partialMockForObject:[UIViewController new]];
 }
 
 - (void)test_setTabBarItemBadge_shouldSetValidValue {
 	NSString* badgeValue = @"badge";
 	[self.uut rnn_setTabBarItemBadge:badgeValue];
-	XCTAssertEqual(self.uut.tabBarItem.badgeValue, badgeValue);
+	XCTAssertEqual([self.uut tabBarItem].badgeValue, badgeValue);
 }
 
 - (void)test_setTabBarItemBadge_shouldResetWhenValueIsEmptyString {
 	[self.uut rnn_setTabBarItemBadge:@"badge"];
 	NSString* badgeValue = @"";
 	[self.uut rnn_setTabBarItemBadge:badgeValue];
-	XCTAssertEqual(self.uut.tabBarItem.badgeValue, nil);
+	XCTAssertEqual([self.uut tabBarItem].badgeValue, nil);
 }
 
 - (void)test_setTabBarItemBadge_shouldResetWhenValueIsNullObject {
 	[self.uut rnn_setTabBarItemBadge:@"badge"];
 	NSNull* nullBadgeValue = [NSNull new];
 	[self.uut rnn_setTabBarItemBadge:nullBadgeValue];
-	XCTAssertEqual(self.uut.tabBarItem.badgeValue, nil);
+	XCTAssertEqual([self.uut tabBarItem].badgeValue, nil);
 }
+
+- (void)testSetDrawBehindTopBarTrue_shouldSetExtendedLayoutTrue {
+    [[self.uut expect] setExtendedLayoutIncludesOpaqueBars:YES];
+    [self.uut rnn_setDrawBehindTopBar:YES];
+    [self.uut verify];
+}
+
+- (void)testSetDrawBehindTabBarTrue_shouldSetExtendedLayoutTrue {
+    [[self.uut expect] setExtendedLayoutIncludesOpaqueBars:YES];
+    [self.uut rnn_setDrawBehindTabBar:YES];
+    [self.uut verify];
+}
+
+- (void)testSetDrawBehindTopBarFalse_shouldNotCallExtendedLayout {
+    [[self.uut reject] setExtendedLayoutIncludesOpaqueBars:NO];
+    [self.uut rnn_setDrawBehindTopBar:NO];
+    [self.uut verify];
+}
+
+- (void)testSetDrawBehindTabBarFalse_shouldNotCallExtendedLayout {
+    [[self.uut reject] setExtendedLayoutIncludesOpaqueBars:NO];
+    [self.uut rnn_setDrawBehindTabBar:NO];
+    [self.uut verify];
+}
+
+- (void)testSetDrawBehindTopBarFalse_shouldSetCorrectEdgesForExtendedLayout {
+	[self.uut rnn_setDrawBehindTopBar:NO];
+    UIRectEdge expectedRectEdge = UIRectEdgeLeft | UIRectEdgeBottom | UIRectEdgeRight;
+    XCTAssertEqual([self.uut edgesForExtendedLayout], expectedRectEdge);
+}
+
+- (void)testSetDrawBehindTapBarFalse_shouldSetCorrectEdgesForExtendedLayout {
+    [self.uut rnn_setDrawBehindTabBar:NO];
+	UIRectEdge expectedRectEdge = UIRectEdgeTop | UIRectEdgeLeft | UIRectEdgeRight;
+	XCTAssertEqual([self.uut edgesForExtendedLayout], expectedRectEdge);
+}
+
+
 
 @end
