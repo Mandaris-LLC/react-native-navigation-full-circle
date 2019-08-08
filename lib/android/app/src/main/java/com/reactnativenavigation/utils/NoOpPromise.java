237c0ead896732551373f1e15d99db8cecda2d14
Alexander Engel <ae94@icloud.com>
2019-08-08 11:25:12 -0700

    fixed NoOPPromise

--- lib/android/app/src/main/java/com/reactnativenavigation/utils/NoOpPromise.java
+++ lib/android/app/src/main/java/com/reactnativenavigation/utils/NoOpPromise.java
@@ -35,4 +35,31 @@ public class NoOpPromise implements Promise {
     public void reject(Throwable reason) {
 
     }
+
+    /*Extra Methods*/
+
+    @Override
+    public void reject(String code, String message, Throwable e, WritableMap a) {
+
+    }
+
+   @Override
+    public void reject(Throwable e, WritableMap a) {
+
+    }
+
+   @Override
+    public void reject(String code, String message, WritableMap a) {
+
+    }
+
+   @Override
+    public void reject(String code, Throwable e, WritableMap a) {
+
+    }
+
+   @Override
+    public void reject(String code, WritableMap a) {
+
+    }
 }
